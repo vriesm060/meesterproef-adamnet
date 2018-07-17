159c484374b2ca569545c93bf808340cb3c9dac0
Max de Vries <max.devries.1995@gmail.com>
2018-07-17 16:40:17 +0200

    Feature/show detail view when clicking photo (#18)
    * Add image detail function
    
    * Remove old image detail function
    
    * Remove old image detail function
    
    * Add data-image and data-text attributes to image
    
    * Add image detail when clicking image in selection box

--- public/js/image-detail.js
+++ public/js/image-detail.js
@@ -1,27 +1,47 @@
+'use strict';
 
+(function () {
 
+  var imageDetail = {
+    trigger: document.querySelectorAll('.openDetail'),
+    detail: document.querySelector('.detail'),
+    detailImg: document.querySelector('.detail img'),
+    detailText: document.querySelector('.detail p'),
+    detailCloseBtn: document.querySelector('.detail .popupCloseButton'),
+    init: function () {
+      var self = this;
 
+      this.trigger.forEach(function (el) {
+        el.addEventListener('click', function (e) {
+          e.preventDefault();
+          self.openDetail(this.dataset.image, this.dataset.text);
+        });
+      });
 
+      this.detailCloseBtn.addEventListener('click', function (e) {
+        e.preventDefault();
+        self.closeDetail();
+      });
 
+      this.detail.addEventListener('click', function (e) {
+        e.preventDefault();
+        self.closeDetail();
+      });
+    },
+    openDetail: function (img, text = '') {
+      // Add image to popup:
+      this.detailImg.src = img;
+      this.detailImg.alt = text;
+      this.detailText.textContent = text;
 
-//open detail page image
-// var trigger = document.querySelectorAll('.openDetail');
-// var detailpopup = document.querySelectorAll('.detail');
-// var closeButton = document.querySelectorAll('.popupCloseButton');
-//
-// for(var i = 0; i < trigger.length; i++) {
-//   trigger[i].addEventListener('click', function(e){
-//     e.preventDefault();
-//     e.path[3].childNodes[3].classList.add('show');
-//   });
-//
-//   detailpopup[i].addEventListener('click', function(e){
-//     e.preventDefault();
-//     e.target.parentElement.classList.remove('show')
-//   });
-//
-//   closeButton[i].addEventListener('click', function(e){
-//     e.preventDefault();
-//     e.target.parentElement.parentElement.classList.remove('show')
-//   });
-// }
+      // Show the popup:
+      this.detail.classList.add('show');
+    },
+    closeDetail: function () {
+      this.detail.classList.remove('show');
+    }
+  };
+
+  imageDetail.init();
+
+})();
