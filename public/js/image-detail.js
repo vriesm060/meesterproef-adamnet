cbc0b999bcbf8509f54bc810d66cebd560cd1829
Max de Vries <max.devries.1995@gmail.com>
2018-07-17 10:45:04 +0200

    Add image-detail.js file and remove image-detail script from create-story.ejs

---
+++ public/js/image-detail.js
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+//open detail page image
+// var trigger = document.querySelectorAll('.openDetail');
+// var detailpopup = document.querySelectorAll('.detail');
+// var closeButton = document.querySelectorAll('.popupCloseButton');
+//
+// for(var i = 0; i < trigger.length; i++) {
+//   trigger[i].addEventListener('click', function(e){
+//     e.preventDefault();
+//     e.path[3].childNodes[3].classList.add('show');
+//   });
+//
+//   detailpopup[i].addEventListener('click', function(e){
+//     e.preventDefault();
+//     e.target.parentElement.classList.remove('show')
+//   });
+//
+//   closeButton[i].addEventListener('click', function(e){
+//     e.preventDefault();
+//     e.target.parentElement.parentElement.classList.remove('show')
+//   });
+// }
