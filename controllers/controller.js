e89d2bd56352a46e1e619ab983fd5ebcac96aa85
Max de Vries <max.devries.1995@gmail.com>
2018-07-18 22:04:19 +0200

    Fix issue with duplicate images in my story page, close #12

--- controllers/controller.js
+++ controllers/controller.js
@@ -148,8 +148,8 @@ exports.postMyStoryPage = function (req, res, next) {
 
   // Push all selected images with meta data in selection array:
   selectedImages.forEach(function (image) {
-    merged.filter(function (item) {
-      if (item.img.value == image) {
+    merged.find(function (item, i) {
+      if (item.img.value == image && i == 0) {
         selection.push(item);
       }
     });
